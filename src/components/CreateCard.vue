20da0d6ce0914b41896bbe944d502f10ed7c4a75
Fabio Rodrigues <fabiordrig@gmail.com>
2020-02-10 11:21:48 -0300

    feat: adding function to creating new cards and saving data in localstorage

--- src/components/CreateCard.vue
+++ src/components/CreateCard.vue
@@ -1,57 +1,66 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="active" persistent max-width="600">
-      <v-card>
-        <v-card-title class="headline">Crie sua nova lista de tarefas:</v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-layout row wrap>
-              <v-flex xs12>
-                <v-radio-group v-model="radioGroup" row>
-                  <v-radio v-for="n in optionList" :key="n" :label="`${n.text}`" :value="n.type"></v-radio>
-                </v-radio-group>
-              </v-flex>
-            </v-layout>
-            <v-layout row>
-              <v-flex grow pa-1>
-                <v-text-field v-model="text" label="Insira a frase da sua tarefa"></v-text-field>
-              </v-flex>
-              <v-flex shrink pa-1>
-                <v-btn @click="addingNewTask" icon>
-                  <v-icon>mdi-plus</v-icon>
-                </v-btn>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-text>
-        <v-card-text>
-          <v-container>
-            <v-layout v-for="(task, index) in tasks" :key="task.id" row>
-              <v-flex grow pa-1>
-                <v-checkbox
-                  v-if="task.type === 'checkbox'"
-                  :label="task.text"
-                  disabled
-                  hide-details
-                ></v-checkbox>
-                <v-text-field v-else v-model="task.text" disabled></v-text-field>
-              </v-flex>
-              <v-flex shrink pa-1>
-                <v-btn @click="excludeTask(index)" icon>
-                  <v-icon>close</v-icon>
-                </v-btn>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="error" text @click="dialog = false">cancelar</v-btn>
-          <v-btn color="success" text @click="dialog = false">Criar Nova Lista</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+  <v-dialog v-model="active" persistent max-width="600">
+    <v-card>
+      <v-card-title class="headline">Crie sua nova lista de tarefas:</v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-layout row>
+            <v-flex grow pa-1>
+              <v-text-field v-model="title" label="Insira o titulo d tarefa"></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout v-if="title" row wrap>
+            <v-flex xs12>
+              <v-radio-group v-model="radioGroup" row>
+                <v-radio
+                  v-for="(n,index) in optionList"
+                  :key="index"
+                  :label="`${n.text}`"
+                  :value="n.type"
+                ></v-radio>
+              </v-radio-group>
+            </v-flex>
+          </v-layout>
+          <v-layout v-if="title" row>
+            <v-flex grow pa-1>
+              <v-text-field v-model="text" label="Insira a frase da sua tarefa"></v-text-field>
+            </v-flex>
+            <v-flex shrink pa-1>
+              <v-btn @click="addingNewTask" icon>
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-card-text>
+        <v-container>
+          <v-layout v-for="(task, index) in tasks" :key="index" row>
+            <v-flex grow pa-1>
+              <v-checkbox v-if="task.type === 'checkbox'" :label="task.text" disabled hide-details></v-checkbox>
+              <v-text-field v-else v-model="task.text" disabled></v-text-field>
+            </v-flex>
+            <v-flex shrink pa-1>
+              <v-btn @click="excludeTask(index)" icon>
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+      <v-snackbar v-model="snackbar" right color="error" timeout="3000">
+        Preencha todos os dados para criar sua lista de tarefas
+        <v-btn dark flat @click="snackbar = false">
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-snackbar>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="error" text @click="active = false">cancelar</v-btn>
+        <v-btn color="success" text @click="saveAllTasks">salvar nova lista</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script>
 export default {
@@ -60,7 +69,9 @@ export default {
   },
   data() {
     return {
-      radioGroup: null,
+      radioGroup: "input",
+      snackbar: false,
+      title: null,
       text: null,
       tasks: [],
       optionList: [
@@ -77,19 +88,55 @@ export default {
   },
   methods: {
     addingNewTask() {
-      let obj = {
-        id: new Date().getTime(),
-        text: this.text,
-        type: this.radioGroup
-      };
+      if (!this.text) {
+        this.snackbar = true;
+        return false;
+      }
 
+      let obj = {
+        id: this.newId(),
+        text: this.text,
+        type: this.radioGroup,
+        state: "NOT_DONE",
+        doneAt: null
+      };
       this.tasks.push(obj);
       this.text = null;
-      this.radioGroup = null;
     },
     excludeTask(index) {
-      console.log(index);
       this.tasks = this.tasks.splice(index, 1);
+    },
+    saveAllTasks() {
+      if (!this.title || !this.radioGroup) {
+        this.snackbar = true;
+        return false;
+      }
+
+      let items = JSON.parse(localStorage.getItem("lists", items));
+
+      let newTask = {
+        id: this.newId(),
+        title: this.title,
+        createAt: new Date(),
+        updatedAt: new Date(),
+        items: this.tasks
+      };
+
+      if (!items) items = [];
+      items.push(newTask);
+
+      localStorage.setItem("lists", JSON.stringify(items));
+      this.title = null;
+      this.text = null;
+      this.tasks = [];
+      this.active = false;
+    },
+    newId() {
+      return "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
     }
   }
 };
