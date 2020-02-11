a088d9a008e1f1a8d5ce21981d7b4946b1a6ae9c
Fabio Rodrigues <fabiordrig@gmail.com>
2020-02-11 13:19:46 -0300

    fix: removing deadcode

--- src/router/appRoutes.ts
+++ src/router/appRoutes.ts
@@ -1,14 +1,6 @@
 const List = () => import(/* webpackChunkName: "about" */ "../views/Cards.vue");
-const CreateCard = () =>
-  import(/* webpackChunkName: "about" */ "../views/CreateCard.vue");
 
 export default {
-  path: "/cards",
-  component: List,
-  children: [
-    {
-      path: "/:id",
-      component: CreateCard
-    }
-  ]
+  path: "/lists",
+  component: List
 };
