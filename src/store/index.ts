01093a20f228c2a154e5fc64685ca28e87094d20
Fabio Rodrigues <fabiordrig@gmail.com>
2020-02-12 18:30:20 -0300

    feat: finish editing card function and add methods to emit events by store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,36 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    refreshData: false
+  },
+  mutations: {
+    refreshData(state) {
+      state.refreshData = !state.refreshData;
+    },
+    excludeCard(state, payload) {
+      let lists = JSON.parse(localStorage.getItem("lists")).filter(
+        task => task.id !== payload
+      );
+      localStorage.setItem("lists", JSON.stringify(lists));
+    },
+    excludeTask(state, { item, callback }) {
+      let tasks = item.tasks;
+      let id = item.id;
+      callback(tasks.filter(task => task.id !== id));
+    }
+  },
   actions: {
     refreshData({ commit }) {
-      console.log(commit, "HELOOO");
-      commit(this.$parent.$emit("refreshData"));
+      commit("refreshData");
+    },
+    excludeCard({ commit }, payload) {
+      commit("excludeCard", payload);
+    },
+    excludeTask({ commit }, payload) {
+      return new Promise(resolve => {
+        commit("excludeTask", { item: payload, callback: resolve });
+      });
     }
   },
   modules: {}
